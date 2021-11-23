f9c5851a3828b059ad587908124baddab5608c52
zhd <709688530@qq.com>
2021-11-23 13:40:05 +0800

    【fix】修改dockerfile文件

--- business/src/main/java/com/example/cloud/operator/file/controller/FileController.java
+++ business/src/main/java/com/example/cloud/operator/file/controller/FileController.java
@@ -4,6 +4,7 @@ import com.example.cloud.data.BaseResult;
 import com.example.cloud.operator.file.facade.FileFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,12 +14,13 @@ import org.springframework.web.multipart.MultipartFile;
  * @since 2021/11/22 14:34
  **/
 @RestController
+@RequestMapping("/upload")
 public class FileController {
 
     @Autowired
     private FileFacade fileFacade;
 
-    @PostMapping("/upload")
+    @PostMapping
     public BaseResult upload(@RequestParam("file") MultipartFile file){
         return BaseResult.succeed(fileFacade.upload(file));
     }
