646247c355c449673bc3613fbf801235f106e187
zhooke <709688530@qq.com>
2022-03-25 14:43:05 +0800

    【fix】修改startup.sh

--- startup.sh
+++ startup.sh
@@ -5,5 +5,5 @@ set -eo pipefail
 modules=( business chatting gateway oauth )
 
 for module in "${modules[@]}"; do
-    docker build -t "${module}:latest" -f /home/admin/application/${module}/
+    docker build -t "${module}:latest" /home/admin/application/${module}/
 done
