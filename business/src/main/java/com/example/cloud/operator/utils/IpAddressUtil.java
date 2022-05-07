a84dfda91387d0bcf02327f715dc80aea9d701a8
zhooke <709688530@qq.com>
2022-05-07 09:40:13 +0800

    【fix】修改获取ip方法

--- business/src/main/java/com/example/cloud/operator/utils/IpAddressUtil.java
+++ business/src/main/java/com/example/cloud/operator/utils/IpAddressUtil.java
@@ -1,18 +1,8 @@
 package com.example.cloud.operator.utils;
 
-import org.apache.commons.lang3.StringUtils;
-
 import javax.servlet.http.HttpServletRequest;
-import java.util.Objects;
 
-/**
- * Create by zhengmm on 2021/4/13 01:47
- */
 public interface IpAddressUtil {
-
-    String UNKNOWN = "unknown";
-    String LOCAL_IP = "127.0.0.1";
-
     /**
      * 获取ip地址
      *
@@ -20,42 +10,36 @@ public interface IpAddressUtil {
      * @return
      */
     static String get(HttpServletRequest request) {
-
-        if (null == request) {
-            return UNKNOWN;
-        }
-
-        String ip = request.getHeader("x-forwarded-for");
-        if (StringUtils.isNotBlank(ip) && !"unknown".equalsIgnoreCase(ip)) {
-            // 多次反向代理后会有多个ip值，第一个ip才是真实ip
-            if (ip.contains(",")) {
-                ip = ip.split(",")[0];
-            }
-            if (LOCAL_IP.equals(ip)) {
-                ip = null;
+        String ip = request.getHeader("X-Forwarded-For");
+        if (ip != null){
+            if (!ip.isEmpty() && !"unKnown".equalsIgnoreCase(ip)) {
+                int index = ip.indexOf(",");
+                if (index != -1) {
+                    return ip.substring(0, index);
+                } else {
+                    return ip;
+                }
             }
         }
-        if (StringUtils.isEmpty(ip) || UNKNOWN.equalsIgnoreCase(ip)) {
-            ip = request.getHeader("X-Real-IP");
+        ip = request.getHeader("X-Real-IP");
+        if (ip != null) {
+            if (!ip.isEmpty() && !"unKnown".equalsIgnoreCase(ip)) {
+                return ip;
+            }
         }
-        if (StringUtils.isEmpty(ip) || UNKNOWN.equalsIgnoreCase(ip)) {
-            ip = request.getHeader("Proxy-Client-IP");
+        ip = request.getHeader("Proxy-Client-IP");
+        if (ip != null) {
+            if (!ip.isEmpty() && !"unKnown".equalsIgnoreCase(ip)) {
+                return ip;
+            }
         }
-        if (StringUtils.isEmpty(ip) || UNKNOWN.equalsIgnoreCase(ip)) {
-            ip = request.getHeader("WL-Proxy-Client-IP");
+        ip = request.getHeader("WL-Proxy-Client-IP");
+        if (ip != null) {
+            if (!ip.isEmpty() && !"unKnown".equalsIgnoreCase(ip)) {
+                return ip;
+            }
         }
-        if (StringUtils.isEmpty(ip) || UNKNOWN.equalsIgnoreCase(ip)) {
-            ip = request.getHeader("HTTP_CLIENT_IP");
-        }
-        if (StringUtils.isEmpty(ip) || UNKNOWN.equalsIgnoreCase(ip)) {
-            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
-        }
-        if (StringUtils.isEmpty(ip) || UNKNOWN.equalsIgnoreCase(ip)) {
-            ip = request.getRemoteAddr();
-        }
-        if(Objects.equals("0:0:0:0:0:0:0:1", ip)){
-            ip = LOCAL_IP;
-        }
-        return StringUtils.isNotBlank(ip) ? ip : UNKNOWN;
+        ip =  request.getRemoteAddr();
+        return ip.equals("0:0:0:0:0:0:0:1") ? "127.0.0.1" : ip;
     }
 }
