87293685d5ac557c8592934e2c19826386c32d3e
zhooke <709688530@qq.com>
2022-12-16 15:00:25 +0800

    【feature】添加照片相关

---
+++ business/src/main/java/com/tensua/operator/photo/mapper/PhotoExifMapper.java
@@ -0,0 +1,16 @@
+package com.tensua.operator.photo.mapper;
+
+import com.tensua.operator.photo.entity.PhotoExif;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 照片信息 Mapper 接口
+ * </p>
+ *
+ * @author zhooke
+ * @since 2022-12-16
+ */
+public interface PhotoExifMapper extends BaseMapper<PhotoExif> {
+
+}
